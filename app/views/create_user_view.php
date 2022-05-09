1cacc2d42860529fab1b4bb463c366417c90a357
zabieru363 <72569208+zabieru363@users.noreply.github.com>
2022-05-09 21:37:54 +0200

    header de la página de creación de usuarios creado

---
+++ app/views/create_user_view.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Crear usuario</title>
+        <link rel="stylesheet" href="../../assets/css/create-user-styles.css">
+    </head>
+    <body>
+        <header>
+            <h1 class="name-app"><i class="fa-solid fa-house"></i> Gestor de usuarios</h1>
+            <button name="Home" class="back-btn"><i class="fa-solid fa-angle-left"></i> Volver</button>
+        </header>
+
+        <!-- FONT AWESOME -->
+        <script src="https://kit.fontawesome.com/833d921ab7.js" crossorigin="anonymous"></script>
+    </body>
+</html>
--- index.php
+++ index.php
@@ -12,41 +12,41 @@
 <body>
     <!-- Header -->
     <header>
-            <h1 class="name-app"><i class="fa-solid fa-house"></i> Gestor de usuarios</h1>
-            <form name="search-form" class="search-form" method="POST" action="<?php echo $_SERVER["PHP_SELF"] ?>">
-                <label for="search">Buscar usuarios <i class="fa-solid fa-magnifying-glass"></i></label>
-                <input type="search" name="searchº">
-            </form>
-            <a href="" class="new-user-btn"><i class="fa-solid fa-circle-plus"></i> Nuevo</a>
-        </header>
+        <h1 class="name-app"><i class="fa-solid fa-house"></i> Gestor de usuarios</h1>
+        <form name="search-form" class="search-form" method="POST" action="<?php echo $_SERVER["PHP_SELF"] ?>">
+            <label for="search">Buscar usuarios <i class="fa-solid fa-magnifying-glass"></i></label>
+            <input type="search" name="searchº">
+        </form>
+        <a href="app/views/create_user_view.php" class="new-user-btn"><i class="fa-solid fa-circle-plus"></i> Nuevo</a>
+    </header>
 
-        <!-- Contenido principal -->
-        <div id="main-flex-container">
-            <table>
-                <th>Seleccionar</th>
-                <th>Usuario</th>
-                <th>Nombre</th>
-                <th>Apellidos</th>
-                <th>Email</th>
-                <th>Fecha registro</th>
-            </table>
+    <!-- Contenido principal -->
+    <div id="main-flex-container">
+        <table>
+            <th>Seleccionar</th>
+            <th>Usuario</th>
+            <th>Nombre</th>
+            <th>Apellidos</th>
+            <th>Email</th>
+            <th>Fecha registro</th>
+        </table>
 
-            <form name="action-form" class="update-form" method="POST" action="<?php echo $_SERVER["PHP_SELF"]?>">
-                <div id="update-form-flex-container">
-                    <label for="users">Actualizar usuario</label><br>
-                    <select name="users">
-                        <option value="">Selecciona un usuario</option>
-                    </select>
-                </div>
-                <button class="update-user-btn" name="Update"><i class="fa-solid fa-pencil"></i> Actualizar usuario</button>
-                <button class="delete-user-btn" name="Delete"><i class="fa-solid fa-trash"></i> Eliminar seleccionados</button>
-            </form>
-        </div>
+        <form name="action-form" class="update-form" method="POST" action="<?php echo $_SERVER["PHP_SELF"] ?>">
+            <div id="update-form-flex-container">
+                <label for="users">Actualizar usuario</label><br>
+                <select name="users">
+                    <option value="">Selecciona un usuario</option>
+                </select>
+            </div>
+            <button class="update-user-btn" name="Update"><i class="fa-solid fa-pencil"></i> Actualizar usuario</button>
+            <button class="delete-user-btn" name="Delete"><i class="fa-solid fa-trash"></i> Eliminar seleccionados</button>
+        </form>
+    </div>
 
-        <!-- Footer -->
-        <footer>
-            Powered by Zabieru © <?= date("Y")?>
-        </footer>
+    <!-- Footer -->
+    <footer>
+        Powered by Zabieru © <?= date("Y") ?>
+    </footer>
     <!-- FONT AWESOME -->
     <script src="https://kit.fontawesome.com/833d921ab7.js" crossorigin="anonymous"></script>
 </body>
