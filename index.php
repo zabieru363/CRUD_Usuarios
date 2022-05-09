3de72f0021c2637a898282807a7f19906c4b7cef
zabieru363 <72569208+zabieru363@users.noreply.github.com>
2022-05-09 14:07:52 +0200

    Hoja de estilos enlazada y font-awesome importado al proyecto

--- index.php
+++ index.php
@@ -5,8 +5,10 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>CRUD de usuarios</title>
+        <link rel="stylesheet" type="text/css" href="assets/css//home-styles.css">
     </head>
     <body>
-        
+        <!-- FONT AWESOME -->
+        <script src="https://kit.fontawesome.com/833d921ab7.js" crossorigin="anonymous"></script>
     </body>
 </html>
